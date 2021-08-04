11cc8bed16ca0a278e09aae07623a558ffbf391f
Edward Gbekie <87101802+eddygzz@users.noreply.github.com>
2021-08-04 23:34:34 +0000

    Create showPicture Function

---
+++ script.js
@@ -0,0 +1,4 @@
+function showPicture(){
+  var image = document.getElementById("hiddenImage")
+  image.setAttribute("src","https://media2.s-nbcnews.com/i/newscms/2019_44/3078571/191031-robert-foster-al-1417_fe8f26c00d09672df77da1a9e8762ecf.jpg ")
+}
